b469d925b7f4e9356caef986ed8e0342f3a582e5
cilegordev <deputyalectro@icloud.com>
2025-01-24 14:03:31 +0700

    make help you to install module to system update script #2 remove /lib/modules/...(kernel version) before install modules

--- done_compile.sh
+++ done_compile.sh
@@ -1,6 +1,7 @@
-sudo make modules_install -j4
-sudo cp -v arch/x86/boot/bzImage /boot/vmlinuz-6.6.64.2-amd64.azl3
-sudo cp -v System.map /boot/System.map-6.6.64.2-amd64.azl3
-sudo cp -v .config /boot/config-6.6.64.2-amd64.azl3
-sudo dracut --force /boot/initramfs-6.6.64.2-amd64.azl3.img
+sudo rm -rfv /lib/modules/6.13.0-amd64.azl3
+sudo make modules_install -j$(nproc)
+sudo cp -v arch/x86/boot/bzImage /boot/vmlinuz-6.13.0-amd64.azl3
+sudo cp -v System.map /boot/System.map-6.13.0-amd64.azl3
+sudo cp -v .config /boot/config-6.13.0-amd64.azl3
+sudo dracut --force /boot/initramfs-6.13.0-amd64.azl3.img
 sudo grub2-mkconfig -o /boot/grub2/grub.cfg
