827d739c059941791795f265f2aaa18435e55a5b
cilegordev <deputyalectro@icloud.com>
2025-02-09 10:46:02 +0700

    make help you to install module to system update script #4 symlink boot/vmlinuz-$(uname -r) from compile kernel folder to modules

--- done_compile.sh
+++ done_compile.sh
@@ -1,7 +1,7 @@
 sudo rm -rfv /lib/modules/6.13.0-amd64.azl3
 sudo make modules_install -j$(nproc)
-sudo ln -sv /kernel/vmlinux /lib/modules/6.13.0-amd64.azl3
 sudo cp -v arch/x86/boot/bzImage /boot/vmlinuz-6.13.0-amd64.azl3
+sudo ln -sv /boot/vmlinuz-6.13.0-amd64.azl3 /lib/modules/6.13.0-amd64.azl3/vmlinuz
 sudo cp -v System.map /boot/System.map-6.13.0-amd64.azl3
 sudo cp -v .config /boot/config-6.13.0-amd64.azl3
 sudo dracut --force /boot/initramfs-6.13.0-amd64.azl3.img
